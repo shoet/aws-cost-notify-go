d6babf0cfd9901a0b8ab10b25183de848b879cda
shoet <32211937+shoet@users.noreply.github.com>
2023-10-24 21:41:27 +0900

    整形

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"os"
+	"strconv"
+	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/costexplorer"
 )
 
-type Cost struct {
-	Tag      string
-	Cost     float64
-	Resource string
+type DayCost struct {
+	Day      *string `json:"day"`
+	Cost     float64 `json:"cost"`
+	Resource string  `json:"resource"`
 }
 
 type CostExplorer struct {
@@ -29,20 +33,22 @@ func NewCostExplorer() (*CostExplorer, error) {
 	}, nil
 }
 
-func (c *CostExplorer) GetCostAndUsage(start string, end string) (*costexplorer.GetCostAndUsageOutput, error) {
+func (c *CostExplorer) GetCostAndUsage(start time.Time, end time.Time) (*costexplorer.GetCostAndUsageOutput, error) {
 	graudarity := aws.String("DAILY")
 	timePeriod := &costexplorer.DateInterval{
-		Start: aws.String("2023-10-22"),
-		End:   aws.String("2023-10-23"),
+		Start: aws.String(start.Format("2006-01-02")),
+		End:   aws.String(end.Format("2006-01-02")),
 	}
 	groupBy := []*costexplorer.GroupDefinition{
 		{
+			// Key:  aws.String("Hoge"),
+			// Type: aws.String("TAG"),
 			Key:  aws.String("SERVICE"),
 			Type: aws.String("DIMENSION"),
 		},
 	}
 	metrics := []*string{
-		aws.String("UnblendedCost"),
+		aws.String("BlendedCost"),
 	}
 
 	input := &costexplorer.GetCostAndUsageInput{
@@ -59,6 +65,25 @@ func (c *CostExplorer) GetCostAndUsage(start string, end string) (*costexplorer.
 	return o, nil
 }
 
+func FormatCost(cost *costexplorer.GetCostAndUsageOutput) ([]*DayCost, error) {
+	res := cost.ResultsByTime
+	var days []*DayCost
+	for _, c := range res {
+		for _, s := range c.Groups {
+			f, err := strconv.ParseFloat(*s.Metrics["BlendedCost"].Amount, 64)
+			if err != nil {
+				return nil, fmt.Errorf("failed to parse float: %v", err)
+			}
+			days = append(days, &DayCost{
+				Day:      c.TimePeriod.Start,
+				Cost:     f,
+				Resource: *s.Keys[0],
+			})
+		}
+	}
+	return days, nil
+}
+
 func main() {
 	// cost explorer
 
@@ -69,28 +94,37 @@ func main() {
 		log.Fatal(err)
 	}
 
-	res, err := c.GetCostAndUsage("2023-10-22", "2023-10-23")
+	now := time.Now()
+	start := time.Date(now.Year(), now.Month(), 1, 0, 0, 0, 0, time.UTC)
+	end := time.Date(now.Year(), now.Month(), now.Day(), 0, 0, 0, 0, time.UTC)
+	res, err := c.GetCostAndUsage(start, end)
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println(res)
+
+	days, err := FormatCost(res)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	b, err := json.MarshalIndent(days, "", "  ")
+	if err != nil {
+		log.Fatal(err)
+	}
+	f, err := os.Create("cost.json")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+	f.Write(b)
 
 }
 
-func FetchCost() []Cost {
-	// fetch cost from aws
-	return []Cost{}
-}
-
-func PostToSlack(costs []Cost) {
-	// post to slack
-}
-
-func FormatCost(costs []Cost) string {
-	// format cost
-	// texts := []string{}
-	// for _, c := range costs {
-	// 	texts = append(texts, "")
-	// }
-	return ""
-}
+// func FetchCost() []Cost {
+// 	// fetch cost from aws
+// 	return []Cost{}
+// }
+//
+// func PostToSlack(costs []Cost) {
+// 	// post to slack
+// }
